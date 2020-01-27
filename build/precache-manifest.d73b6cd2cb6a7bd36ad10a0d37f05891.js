63047558d082e8000ad50dadbea8f701d945d019
fischsauce <fishsauce@protonmail.com>
2020-01-27 07:08:55 +0000

    forked a production branch

---
+++ build/precache-manifest.d73b6cd2cb6a7bd36ad10a0d37f05891.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3436d392c871e4cfca9da4c8164c557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d444d17d5dc0f1c19bf",
+    "url": "/static/css/2.e3795789.chunk.css"
+  },
+  {
+    "revision": "0cc1a8ff6d0d487503fc",
+    "url": "/static/css/main.42c6bb25.chunk.css"
+  },
+  {
+    "revision": "9d444d17d5dc0f1c19bf",
+    "url": "/static/js/2.097bfd5c.chunk.js"
+  },
+  {
+    "revision": "7deefc65ba0e98cdfa1ec9842b9ea0af",
+    "url": "/static/js/2.097bfd5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cc1a8ff6d0d487503fc",
+    "url": "/static/js/main.6624a9c1.chunk.js"
+  },
+  {
+    "revision": "79002ff2ee9dbb8f3255",
+    "url": "/static/js/runtime-main.74340f24.js"
+  },
+  {
+    "revision": "07bee5fab940b9c7df45556a8823c172",
+    "url": "/static/media/Frontpagebird.07bee5fa.png"
+  }
+]);
